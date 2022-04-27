fd6ac96cc7e217b828736c08408c828611948a58
Nick Emmerson <nickemmerson619@hotmail.com>
2022-04-27 21:46:56 +0100

    How to reduce 20+ lines of shell script to 2

--- Scripts/solution_count_V2.sh
+++ Scripts/solution_count_V2.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-SLN_COUNT=0
-# If an argument provided to this script, switch to this directory.
+# solution_count.sh can be expressed in 2 lines
 [ -n "$1" ] && cd $1
-for i in ls -R
-do
-# Something in here???
-done
-echo "Total Solutions : $SLN_COUNT"
+ls -R | grep '\.sln$'
