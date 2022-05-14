edf46c67ef74ebfc98de837a355a0b1a7a735c58
Nick Emmerson <nickemmerson619@hotmail.com>
2022-05-14 21:51:01 +0100

    This should work now

--- Scripts/workshy/workshy.sh
+++ Scripts/workshy/workshy.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+
+# Function for inserting lines into script
+insert_vars()
+{
+    LINE=0
+    while read NEXT
+    do
+        if [ $LINE -eq 1 ]; then
+            echo "TARGET_NAME=\"$1\""
+            echo "TARGET_EMAIL=\"$2\""
+        fi
+        echo $NEXT
+        LINE=`expr $LINE + 1`
+    done
+}
+
+# Main script
 [ $# -eq 0 ] && exit 1
 [ "$1" != "-n" ] && exit 1
 shift
@@ -23,18 +40,13 @@ git config --global user.name "$NAME"
 git config --global user.email "$EMAIL"
 # For each remaining argument, check the folder exists within user directory, if not create it.
 HOOKS="$USERPROFILE"
-cd "$HOOKS"
 while [ $# -gt 0 ]
 do
-    [ ! -d "$1" ] && mkdir "$1"
-    cd "$1"
     HOOKS="$HOOKS""\\""$1"
+    [ ! -d "$HOOKS" ] && mkdir "$HOOKS"
     shift
 done
 git config --global core.hooksPath "$HOOKS"
-SCRIPT_FILE=`date +%s`
-# Nearly at the end now.
-# Need to create the commit-msg file and copy to hooks folder.
-# Didn't notice I changed directory above inside the script.
+cat ./Scripts/workshy/V1.sh | insert_vars "$NAME" "$EMAIL" >> "$HOOKS\\commit-msg"
 # Old script below
 # cp -T ./Scripts/workshy/V1.sh "$USERPROFILE\\Git\\Hooks\\commit-msg"
