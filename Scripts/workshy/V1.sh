aca06e97a01bb5148f40b607a5f1041341fd8ec8
Nick Emmerson <nickemmerson19@hotmail.con>
2022-05-14 02:12:35 +0100

    Better version of workshy

--- Scripts/workshy/V1.sh
+++ Scripts/workshy/V1.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
-# This is a git commit-msg hook, checks if you have the right name/email
-check_user_details()
-{
-  # Set the correct details here
-  YOUR_NAME="${1:?"Name must be supplied"}"
-  YOUR_EMAIL="${2:?"Email must be supplied"}"
-  # Get the current user details
-  CURRENT_NAME=`git config user.name`
-  CURRENT_EMAIL=`git config user.email`
-  # Check they match, if not prompt
-  [ "$CURRENT_NAME" = "$YOUR_NAME" -a "$CURRENT_EMAIL" = "$YOUR_EMAIL" ] && \
-    (
-      echo "Correct user details found, committing..."
-      exit 0
-    ) ||
-    (
-      echo "Hey dude, your user details looks incorrect."
-      exit 1
-    )
-}
-
-check_user_details "Nick Emmerson" "nickemmerson619@hotmail.com"
+CURRENT_NAME=`git config user.name`CURRENT_EMAIL=`git config user.email`
+# Check they match, if not prompt
+[ "$CURRENT_NAME" = "$TARGET_NAME" -a "$CURRENT_EMAIL" = "$TARGET_EMAIL" ] && \
+  (
+    echo "Correct user details found, committing..."
+    exit 0
+  ) ||
+  (
+    echo "Hey dude, your user details looks incorrect."
+     exit 1
+  )
--- Scripts/workshy/workshy.sh
+++ Scripts/workshy/workshy.sh
@@ -1,3 +1,40 @@
 #!/bin/sh
-# Works as long as your user gitconfig points to here for hooks.
-cp -T ./Scripts/workshy/V1.sh "$USERPROFILE\\Git\\Hooks\\commit-msg"
+[ $# -eq 0 ] && exit 1
+[ "$1" != "-n" ] && exit 1
+shift
+[ "$1" = "-e" ] && exit 1
+NAME="$1"
+shift
+while [ $# -gt 0 -a "$1" != "-e" ]
+do
+    NAME="$NAME"" ""$1"
+    shift
+done
+[ $# -eq 0 ] && exit 1
+shift
+EMAIL="$1"
+shift
+[ "$1" != "-h" ] && exit 1
+shift
+[ $# -eq 0 ] && exit 1
+# At this point, remaining arguments are all hook folders.
+# Update .gitconfig user file.
+git config --global user.name "$NAME"
+git config --global user.email "$EMAIL"
+# For each remaining argument, check the folder exists within user directory, if not create it.
+HOOKS="$USERPROFILE"
+cd "$HOOKS"
+while [ $# -gt 0 ]
+do
+    [ ! -d "$1" ] && mkdir "$1"
+    cd "$1"
+    HOOKS="$HOOKS""\\""$1"
+    shift
+done
+git config --global core.hooksPath "$HOOKS"
+SCRIPT_FILE=`date +%s`
+# Nearly at the end now.
+# Need to create the commit-msg file and copy to hooks folder.
+# Didn't notice I changed directory above inside the script.
+# Old script below
+# cp -T ./Scripts/workshy/V1.sh "$USERPROFILE\\Git\\Hooks\\commit-msg"
