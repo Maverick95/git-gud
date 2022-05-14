0b97e27dbaac08b3b754a49239fd124855ed6cae
Nick Emmerson <nickemmerson619@hotmail.com>
2022-05-14 21:52:24 +0100

    This should also work

--- Scripts/workshy/V1.sh
+++ Scripts/workshy/V1.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 [ -z "$TARGET_NAME" -o -z "$TARGET_EMAIL" ] && exit 1
-CURRENT_NAME=`git config user.name`CURRENT_EMAIL=`git config user.email`
+CURRENT_NAME=`git config user.name`
+CURRENT_EMAIL=`git config user.email`
 # Check they match, if not prompt
 [ "$CURRENT_NAME" = "$TARGET_NAME" -a "$CURRENT_EMAIL" = "$TARGET_EMAIL" ] && \
   (
