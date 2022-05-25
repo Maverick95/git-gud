37b431781809a2f95918657f3b96a0901146ad90
Nick Emmerson <nickemmerson619@hotmail.com>
2022-05-25 23:57:54 +0100

    First stab at AWK using GNU AWK docs

--- Scripts/base64guid/v1.sh
+++ Scripts/base64guid/v1.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-# This works but seems horribly slow.
 echo "$1" | \
 tr -t "[:upper:]" "[:lower:]" | \
 (
   read INPUT
   if [ ! `expr "$INPUT" : '[0-9a-f]\{8\}\-[0-9a-f]\{4\}\-[0-9a-f]\{4\}\-[0-9a-f]\{4\}\-[0-9a-f]\{12\}'` -eq 36 ]; then
-    echo "GUID must be supplied"
     exit 1
   fi
   echo "$INPUT"
