828409c17571f86d4d9d4031f86a432da45d6f05
Nick Emmerson <nickemmerson619@hotmail.com>
2022-09-12 15:27:10 +0100

    Make pathfinder great again

--- Scripts/pathfinder/V1.sh
+++ Scripts/pathfinder/V1.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
-# This will list the name of a .exe if it exists in your path.
+
+SED_FIND="\
+\(.*\)\
+"
+SED_REPLACE="\
+[ -d \"\1\" ] \
+\&\& \
+echo \"\1\"\
+"
+
 echo "$PATH" |
 tr [=:=] "\n" |
+sed "s/$SED_FIND/$SED_REPLACE/;e" |
+grep ".\+" |
 xargs -I "{}" ls -1 --color=no --file-type "{}" |
 grep -i "$1.exe$" |
 sort
