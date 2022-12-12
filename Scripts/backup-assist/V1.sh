83546098149dbcd045f3b3c5efdee2c60b00b51d
Nick Emmerson <nickemmerson619@hotmail.com>
2022-12-12 00:24:43 +0000

    Fix backup script

--- Scripts/backup-assist/V1.sh
+++ Scripts/backup-assist/V1.sh
@@ -3,6 +3,6 @@
 # backing up node_modules folders inside npm package projects,
 # which makes it take f***ing ages.
 # This is designed to delete all those folders.
-find "c:/users/nick/documents/programming" -name "package.json" |
-sed "s/\(.*\)\/package.json$/\1\/node_modules/;" |
-xargs -I "{}" [ -d "{}" ] && rm -fiR "{}"
+find "$1" -name "package.json" |
+sed "s/\(.*\)\/package.json$/\1\/node_modules/" |
+rm -fdR
