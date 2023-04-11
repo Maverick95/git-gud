615e1fa850adf3effe00a727785b48d039af3361
Nick Emmerson <nickemmerson619@hotmail.com>
2023-04-11 22:29:22 +0100

    good-migrations script

--- Scripts/good-migrations/V1.sh
+++ Scripts/good-migrations/V1.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-# Underneath a given directory, find all .cs files that are under a Migrations folder.
-# Files must obey the Entity format.
+# Underneath a given directory, find all migration code files.
 [ ! -d "$1" ] && exit 1
+find "$1" -regextype 'posix-egrep' -iregex '.*/migrations/[0-9]{14,15}_.*\.cs' -not -ipath '*/bin/*'
+# Would be a good time to look at awk.
